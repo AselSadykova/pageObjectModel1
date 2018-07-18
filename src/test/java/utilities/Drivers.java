c4e1e15ca985d76ab8d73ecd4e388e1d0e5f86a3
Asel Sadykova <aselsadykova61@gmail.com>
2018-07-18 17:11:07 -0500

    added some chamges

--- src/test/java/utilities/Drivers.java
+++ src/test/java/utilities/Drivers.java
@@ -13,6 +13,7 @@ public class Drivers {
 public static WebDriver getDriver() {
 	 if(driver != null) {
 		 //comment another comment
+		 //comment
          return driver;  // if the driver object was created before, 
          //it’s not null so it will return driver and the code will be terminated 
 	 }
